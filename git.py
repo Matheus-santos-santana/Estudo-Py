f7f61f3ab20e249c3d0ff30d392b47ba7f56d407
Matheus dos Santos Santana <matheus.santana@humaniasaude.com.br>
2024-12-12 20:04:19 -0300

    Criação de Projeto git

--- git.py
+++ git.py
@@ -1,1 +1,11 @@
-print("Boa tarde, esse é um teste de git")
+nome = input("Estimo que esteja bem, favor, digite seu nome:")
+
+resposta = input (f"Olá {nome}, você está entrando em um projeto de teste para Commit Você deseja continuar?")
+
+if resposta.lower() == "sim":
+    print ("Quem bom que deseja continuar, é um prazer tê-lo nesse projeto")
+
+elif resposta.lower() == "não":
+    print ("ótimo, agradecemos a sua curiosidade, obrigado!")
+
+else: ("Resposta invalida, favor responder 'sim' ou 'não', obrigado")
