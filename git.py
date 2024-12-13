95f3132f27460816b91492454f3ca07eb4bb2aff
Matheus dos Santos Santana <matheus.santana@humaniasaude.com.br>
2024-12-12 22:17:08 -0300

    criação de interação entre sistema e usuário, caso o usuário seja maior de idade, terá acesso ao projeto ao criar um acesso, caso seja menor de idade, será encerrado a interação!

--- git.py
+++ git.py
@@ -1,11 +1,41 @@
-nome = input("Estimo que esteja bem, favor, digite seu nome:")
+def cadastrar_usuário():
+    usuario = input("Crie um usuário para ter acesso ao projeto: ")
+    senha = input("Crie uma senha: ")
+    return usuario, senha
 
-resposta = input (f"Olá {nome}, você está entrando em um projeto de teste para Commit Você deseja continuar?")
+def fazer_login(usuario_cadastro, senha_cadastrada):
+    usuario = input("Digite seu nome de usuário: ")
+    senha = input("Digite a sua senha: ")
 
-if resposta.lower() == "sim":
-    print ("Quem bom que deseja continuar, é um prazer tê-lo nesse projeto")
+    if usuario == usuario_cadastro and senha == senha_cadastrada:
+        print("Acesso Concedido!")
+    else:
+        print("Usuário ou senha incorretos. Tente novamente.")
 
-elif resposta.lower() == "não":
-    print ("ótimo, agradecemos a sua curiosidade, obrigado!")
+def main():
+    nome = input("Estimo que esteja bem, favor, digite seu nome: ")
 
-else: ("Resposta invalida, favor responder 'sim' ou 'não', obrigado")
+    resposta = input(f"Olá {nome}, você está entrando em um projeto de teste para Commit. Você deseja continuar? ")
+
+    if resposta.lower() == "sim":
+        idade = input("Que bom que deseja continuar, é um prazer tê-lo nesse projeto. Quantos anos você tem? ")
+        idade = int(idade)
+
+        if idade >= 18:
+            print("Você é maior de idade, pode prosseguir com o projeto!")
+            usuario_cadastro, senha_cadastrada = cadastrar_usuário()
+
+            print("\nUsuário cadastrado com sucesso!")
+            print("Agora tente acessar o projeto com seu login.\n")
+            fazer_login(usuario_cadastro, senha_cadastrada)
+        else:
+            print("Você é menor de idade, infelizmente não pode prosseguir.")
+
+    elif resposta.lower() == "não":
+        print("Ótimo, agradecemos a sua curiosidade. Obrigado!")
+
+    else:
+        print("Resposta inválida, favor responder 'sim' ou 'não', obrigado.")
+
+
+main()
